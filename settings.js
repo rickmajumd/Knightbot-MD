7599bbbabf9dd4a0b4edfe05caa05c680d7d64b4
rickmajumd <rickmajumdar51@gmail.com>
2025-04-26 00:03:38 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Knight',
+  packname: '🐰⃝»̶̶͓͓͓̽̽̽⑅⃝༊𝐂𝐑𝐔𝐒𝐇 𝗞𝗜𝗡𝗚 »̶̶͓͓͓̽̽̽⑅⃝🩵🪽',
   author: 'Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '91702395151X', //Set your number here without + symbol, just add country code & number without any space
+  botName: "🐰⃝»̶̶͓͓͓̽̽̽⑅⃝༊𝐂𝐑𝐔𝐒𝐇 𝗞𝗜𝗡𝗚 »̶̶͓͓͓̽̽̽⑅⃝🩵🪽",
+  botOwner: '🦋⃟ᴠͥɪͣᴘͫ✮⃝R𝄟⃝I✮⃝C𝄟⃝K', // Your name
+  ownerNumber: '919830858093', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
